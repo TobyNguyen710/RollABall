42b0a7e88c89cf62d1f0701a9047d6ae77a73628
TobyNguyen710 <ductoby2003@gmail.com>
2025-01-18 16:02:12 -0800

    Refactor Input Code

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,23 +13,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector= Vector2.zero;//intializeourinput vector
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector +=Vector2.up; //"a +=b" <=>"a =a +b"
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector +=Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector +=Vector2.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector +=Vector2.left;
-        }
+        
+    }
+    public void MoveBall(Vector2 inputVector)
+    {
         Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
         Debug.Log("Resultant Vector: " + inputVector);
         Debug.Log("Resultant 3D Vector: " + inputVector);
